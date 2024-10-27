d186f3849d42747d7d31103d0128528636f8c2b1
nagomiita <konnichiwatakeshi@gmail.com>
2024-10-27 22:56:51 +0900

    完成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,42 +5,95 @@ import "@/App.css";
 interface Character {
   id: number;
   images: string;
+  name: string;
+  debut: { appearsIn: string };
+  personal: { species: string };
 }
 
 const App = () => {
   const [characters, setCharacters] = useState<Character[]>([]);
-  useEffect(() => {
-    fetchCharacters();
-  }, []);
-  const fetchCharacters = async () => {
-    const apiUrl = "https://narutodb.xyz/api/character";
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const limit = 800;
 
-    const result = await axios.get(apiUrl);
-    setCharacters(result.data.characters);
+  useEffect(() => {
+    fetchCharacters(page);
+  }, [page]);
+  const fetchCharacters = async (page: number) => {
+    const apiUrl = `https://narutodb.xyz/api/character`;
+    setIsLoading(true);
+    const result = await axios.get(apiUrl, {
+      params: { page: page, limit: limit },
+    });
     console.log(result);
+    setCharacters(result.data.characters);
+    setIsLoading(false);
+  };
+  const handleNext = async () => {
+    const nextPage = page + 1;
+    await fetchCharacters(nextPage);
+    setPage(nextPage);
+  };
+  const handlePrevious = async () => {
+    if (page > 0) {
+      const nextPage = page - 1;
+      await fetchCharacters(nextPage);
+      setPage(nextPage);
+    }
   };
   return (
     <div className="container">
-      <main>
-        <div className="cards-container">
-          {characters.map((character) => {
-            return (
-              <div className="card" key={character.id}>
-                <img
-                  src={
-                    character.images[0] != null
-                      ? character.images[0]
-                      : "dummy.png"
-                  }
-                  alt="character"
-                  className="card-image"
-                />
-              </div>
-            );
-          })}
+      <div className="header">
+        <div className="header-content">
+          <img src="logo.png" alt="logo" className="logo" />
         </div>
-      </main>
-      App
+      </div>
+      {isLoading ? (
+        <div>Now Loading...</div>
+      ) : (
+        <main>
+          <div className="cards-container">
+            {characters.map((character) => {
+              return (
+                <div className="card" key={character.id}>
+                  <img
+                    src={
+                      character.images[0] != null
+                        ? character.images[0]
+                        : "dummy.png"
+                    }
+                    alt="character"
+                    className="card-image"
+                  />
+                  <div className="card-content">
+                    <h3 className="card-title">{character.name}</h3>
+                  </div>
+                  {character.debut?.appearsIn}
+                  <p className="card-description"></p>
+                  <div className="card-footer">
+                    {character.personal?.species}
+                    <span className="affiliation"></span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="pager">
+            <button
+              className="prev"
+              onClick={handlePrevious}
+              disabled={page === 1}
+            >
+              Previous
+            </button>
+
+            <span className="page-number">{page}</span>
+            <button className="next" onClick={handleNext} disabled={page === 2}>
+              Next
+            </button>
+          </div>
+        </main>
+      )}
     </div>
   );
 };
