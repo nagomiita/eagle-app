ab70e9cded40f2d203af1f050df5623976ca72e2
nagomiita <konnichiwatakeshi@gmail.com>
2025-07-28 00:20:37 +0900

    ローディング実装

--- frontend/src/components/ImageGallery.tsx
+++ frontend/src/components/ImageGallery.tsx
@@ -51,9 +51,10 @@ const ImageGrid: React.FC = () => {
 
   const LoadingIndicator = () => (
     <div className="flex justify-center items-center h-64">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900 dark:border-white"></div>
+      <div className="animate-spin rounded-full h-16 w-16 border-4 border-gray-300 dark:border-gray-700 border-t-blue-500"></div>
     </div>
   );
+  console.log(isLoading);
 
   if (isLoading) {
     return <LoadingIndicator />;
