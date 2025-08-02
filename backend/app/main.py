76471efb0d1b63e19d4dd8c07fcf1878c5642635
nagomiita <konnichiwatakeshi@gmail.com>
2025-08-02 14:31:39 +0900

    gzip化

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from app.routers import folders, images, tags
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(root_path="/api")
@@ -12,6 +13,8 @@ app.mount(
     StaticFiles(directory=static_dir, follow_symlink=True),
     name="static",
 )
+
+app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 app.add_middleware(
     CORSMiddleware,
