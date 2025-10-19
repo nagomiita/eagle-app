be41431b08d12d945d5d39073f7ec383ef64b103
nagomiita <konnichiwatakeshi@gmail.com>
2025-10-19 13:44:04 +0900

    静的ファイル配信を廃止

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,19 @@
-from pathlib import Path
+# from pathlib import Path
 
 from app.routers import folders, images, tags
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
-from fastapi.staticfiles import StaticFiles
+
+# from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(root_path="/api")
-static_dir = Path(__file__).resolve().parent.parent / "thumbnails"
-app.mount(
-    "/static/thumbnails",
-    StaticFiles(directory=static_dir, follow_symlink=True),
-    name="static",
-)
+# static_dir = Path(__file__).resolve().parent.parent / "thumbnails"
+# app.mount(
+#     "/static/thumbnails",
+#     StaticFiles(directory=static_dir, follow_symlink=True),
+#     name="static",
+# )
 
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
